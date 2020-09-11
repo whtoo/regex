e18a974368b84af8ee83924525d47d1b747e98a7
whtoo <whtoo@qq.com>
2020-09-11 21:17:59 +0800

    1. 增加写作计划大纲；2. java实现。

--- java/toy/src/main/java/org/blitz/regex/NFA.java
+++ java/toy/src/main/java/org/blitz/regex/NFA.java
@@ -1,5 +1,56 @@
 package org.blitz.regex;
 
 public class NFA {
-    
+    private State start;
+    private State end;
+
+    public NFA(State ss,State es){
+        this.start = ss;
+        this.end = es;
+    }
+
+    @Override
+    public int hashCode() {
+        return (this.start.toString() + this.end.toString()).hashCode() ;
+    }
+
+    /**
+     * n1 . n2
+     * @param n1
+     * @param n2
+     */
+    public NFA concat(NFA n1,NFA n2){
+        // TODO concat
+        return n1;
+    }
+
+    /**
+     * n1*
+     * @param n1
+     */
+    public NFA cleen(NFA n1){
+        // TODO cleen
+        return n1;
+    }
+    /**
+     * n1 | n2
+     * @param n1
+     * @param n2
+     * @return
+     */
+    public NFA or(NFA n1,NFA n2) {
+        // TODO or
+        return n1;
+    }
+
+    /**
+     * n1?
+     * @param n1
+     */
+    public NFA option(NFA n1) {
+        // TODO option
+        return n1;
+    }
+
+
 }
---
+++ java/toy/src/main/java/org/blitz/regex/State.java
@@ -0,0 +1,41 @@
+package org.blitz.regex;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class State {
+    static Integer IDGENERATOR = 0;
+    Integer id;
+    List<State> epsilonTransitions;
+    Map<String,State> transitions;
+    Boolean isEnd;
+    
+    public State() {
+        this.id = State.IDGENERATOR++;
+        this.isEnd = false;
+        this.epsilonTransitions = new ArrayList<>();
+        this.transitions = new HashMap<>();
+    }
+
+    public static void resetIDGenerator() {
+        State.IDGENERATOR = 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int hashCode = this.id & 17;
+        hashCode ^= this.epsilonTransitions.hashCode();
+        hashCode ^= this.transitions.hashCode();
+        return hashCode;
+    }
+
+    public void addEpsilonTransition(State st){
+        this.epsilonTransitions.add(st);
+    }
+
+    public void addTransition(String key,State st){
+        this.transitions.put(key, st);
+    }
+}
